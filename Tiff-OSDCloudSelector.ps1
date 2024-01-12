b3612417efb6a328030f48d2f8cd133ca51463f6
nelsonaustin <149835342+nelsonaustin@users.noreply.github.com>
2024-01-12 13:55:14 -0600

    Update Tiff-OSDCloudSelector.ps1
    Updated Win11 build to 23H2

--- Tiff-OSDCloudSelector.ps1
+++ Tiff-OSDCloudSelector.ps1
@@ -3,7 +3,7 @@ cls
 Write-Host "===================== Main Menu =======================" -ForegroundColor Yellow
 Write-Host "=================== Tiffany & Co. =====================" -ForegroundColor Yellow
 Write-Host "=======================================================" -ForegroundColor Yellow
-Write-Host "1: Zero-Touch Win11 22H2 | English | Enterprise" -ForegroundColor Yellow
+Write-Host "1: Zero-Touch Win11 23H2 | English | Enterprise" -ForegroundColor Yellow
 Write-Host "2: Zero-Touch Win10 22H2 | English | Enterprise" -ForegroundColor Yellow
 Write-Host "3: Zero-Touch Win10 20H2 | English | Enterprise" -ForegroundColor Yellow
 Write-Host "4: I'll select it myself"-ForegroundColor Yellow
@@ -17,7 +17,7 @@ Write-Host  -ForegroundColor Yellow "Loading OSDCloud..."
 
 switch ($input)
 {
-    '1' { Start-OSDCloud -OSLanguage en-us -OSBuild 22H2 -OSVersion 'Windows 11' -OSEdition Enterprise -ZTI } 
+    '1' { Start-OSDCloud -OSLanguage en-us -OSBuild 23H2 -OSVersion 'Windows 11' -OSEdition Enterprise -ZTI } 
     '2' { Start-OSDCloud -OSLanguage en-us -OSBuild 22H2 -OSVersion 'Windows 10' -OSEdition Enterprise -ZTI }
     '3' { Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSVersion 'Windows 10' -OSEdition Enterprise -ZTI } 
     '4' { Start-OSDCloud	} 
